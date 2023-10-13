a2ee57d4217c712d87c90022310a1ed131ee8cb8
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-10-13 11:57:00 +0900

    Test on rails 7.1 and postgres 15

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,6 +9,8 @@ require "activerecord_nested_scope"
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults Rails::VERSION::STRING.to_f if config.respond_to?(:load_defaults)
+
     unless (database = ENV['DATABASE'].to_s).empty?
       config.paths["config/database"] = "config/database_#{database}.yml"
       ENV['SCHEMA'] = Rails.root.join("db/schema_#{database}.rb").to_s
