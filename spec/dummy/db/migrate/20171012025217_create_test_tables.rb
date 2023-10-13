860c7c4ceaae05cf7a3b214408f69c7a59acfd7b
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-10-13 11:18:01 +0900

    Fix migration version for test

--- spec/dummy/db/migrate/20171012025217_create_test_tables.rb
+++ spec/dummy/db/migrate/20171012025217_create_test_tables.rb
@@ -1,4 +1,4 @@
-class CreateTestTables < ActiveRecord::Migration[5.0]
+class CreateTestTables < ActiveRecord::Migration::Current
   def change
     create_table :organizations do |t|
       t.text       :title
